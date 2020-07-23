31050742de4c623fbce6a93d42d8bc24034e6ee0
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2020-07-23 10:13:49 -0400

    Added untracked files

--- FontIndexer/CommandLineArguments.cs
+++ FontIndexer/CommandLineArguments.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommandLine;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,12 @@ namespace FontIndexer
 {
     class CommandLineArguments
     {
+        [Option("source", Required = true, HelpText = "Source directory")]
+        public string SourceDirectory { get; set; }
+
+        [Option("output", Required = true, HelpText = "Output file (.json)")]
+        public string OutputFile { get; set; }
+        [Option('r', HelpText = "Whether to recursively look through a directory")]
+        public bool Recursive { get; set; }
     }
 }
